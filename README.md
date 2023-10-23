b3ea48ca35583ffa408b8a28eb168970fb1848d4
savhsergio <sergioandresherrera@gmail.com>
2023-10-22 21:55:38 -0500

    practiced adding FLASK_DEBUG as an environment variable

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # flask-app-Platzi-course
+
 # flask-app-Platzi-course
+
+#FLASK_APP: this is the variable meant to identify the file where the application is located
+
+# in the terminal just write export FLASK_APP=main.py on linux, set FLASK_APP=main.py on Windows and on powershell $env:FLASK_APP = ‘main.py’
+
+#to check, just run echo $FLASK_APP, it should return main.py
+#type "flask run"
+
+#para los que trabajan con anaconda:
+
+#--> Para crearlo
+
+#conda create -n NombreEntorno python=3.9
+
+#Eligen la versión de python que deseen.
+
+#--> Para activarlo
+
+#conda activate NombreEntorno
+
+#--> Para desactivarlo:
+
+#conda deactivate
+#Cuando se utilize conda en lugar de pip este comando guarda los comandos de creacion del ambiente:
+
+#conda env export --from-history --file environment.yml.
+export FLASK_DEBUG=1
+echo $FLASK_DEBUG
--- main.py
+++ main.py
@@ -1,30 +1,8 @@
 from flask import Flask
 
-app=Flask(__name__)
-#FLASK_APP: this is the variable meant to identify  the file where the application is located
-# in the terminal just write export FLASK_APP=main.py on linux, set FLASK_APP=main.py on Windows and on powershell $env:FLASK_APP = ‘main.py’ 
-#to check, just run echo $FLASK_APP, it should return main.py
-#type "flask run"
+app = Flask(__name__)
 
-#para los que trabajan con anaconda:
-
-#--> Para crearlo
-
-#conda create -n NombreEntorno python=3.9
-
-#Eligen la versión de python que deseen.
-
-#--> Para activarlo
-
-#conda activate NombreEntorno
-
-#--> Para desactivarlo:
-
-#conda deactivate
-#Cuando se utilize conda en lugar de pip este comando guarda los comandos de creacion del ambiente:
-
-#conda env export --from-history --file environment.yml
 
 @app.route('/')
 def hello():
-    return'Hello World Colombia'
+    return 'Hello World Colombia'

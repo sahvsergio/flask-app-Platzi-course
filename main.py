6b3b9d60ba2d98a32819f8464afc9a5859af2789
savhsergio <sergioandresherrera@gmail.com>
2023-10-31 21:15:30 -0500

    flask bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, make_response,redirect,render_template
-
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap=Bootstrap(app)
+
 todos=['TODO 1','TODO 2', 'TODO 3']
 
 @app.route('/')

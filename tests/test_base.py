78c8c73cb42a345553b543b99a88a1c80ccdb894
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-03-28 20:19:34 -0500

    added blueprints

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,37 +1,52 @@
+import unittest
 from flask_testing import TestCase
 from flask import current_app, url_for
 
 from main import app
 
+
 class MainTest(TestCase):
     def create_app(self):
-        app.config['TESTING']=True
-        app.config['WTF_CSRF_ENABLED']=False
-        return app 
-    
-    
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        return app
+
     def test_app_exist(self):
         self.assertIsNotNone(current_app)
-        
+
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
-    
+
     def test_index_redirects(self):
-        response=self.client.get(url_for('index'))
-        
-        self.assertRedirects(response,url_for('hello') )
-    
+        response = self.client.get(url_for('index'))
+
+        self.assertRedirects(response, url_for('hello'))
+
     def test_hello_get(self):
-        response=self.client.get(url_for('hello'))
+        response = self.client.get(url_for('hello'))
         self.assert200(response)
-    
+
     def test_hello_post(self):
-        fake_form={
-            'username':'fake',
-            'password':'fake-password'
-            
-            
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+
+
         }
-        response=self.client.post(url_for('hello'),data=fake_form)
+        response = self.client.post(url_for('hello'), data=fake_form)
         self.assertRedirects(response, url_for('index'))
-        
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+        self.assert200(response)
+
+    def test_auth_login_get(self):
+        self.client.get(url_for('auth.login'))
+        self.assertTemplateUsed('login.html')
+
+
+if __name__ == '__main__':
+    unittest.main()
